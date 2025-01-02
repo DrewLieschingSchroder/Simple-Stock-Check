fc53a8f3eeec95e1c79222d491086fd3229ea694
Drew <drewliesching@gmail.com>
2025-01-02 17:49:42 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import streamlit as st
 from datetime import datetime as dt
 import pandas as pd
 
-st.write('Simple Stock Price App')
+st.write('# Simple Stock Price App')
 
+# user inputs the ticker symbol of the company they want to see the stock price for
 user_input = st.text_input("Enter a Stock Ticker Symbol (e.g: AAPL, GOOGL, NVDA) - ")
 
+# define the ticker symbol
 tickerSymbol = user_input
 
+# gets the data (from yahoo finance) for this ticker
 tickerData = yf.Ticker(tickerSymbol)
 
+# gets historical prices for the ticker
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end=dt.today().strftime('%Y-%m-%d'))
 
-st.write(tickerData.info['longName'])
+longName = tickerData.info['longName']
+st.write(f"## {longName}")
 
+st.write("## Closing Price")
 st.line_chart(tickerDf.Close)
-volume = st.line_chart(tickerDf.Volume)
 
+st.write("## Volume Price")
+volume = st.line_chart(tickerDf.Volume)
 
